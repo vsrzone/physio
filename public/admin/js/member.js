4085ead420de35311512e1ab46e13c1136354a15
pulasthilakshan <pulasthilakshan@gmail.com>
2015-04-06 13:17:46 +0530

    member registration done

---
+++ public/admin/js/member.js
@@ -0,0 +1,2 @@
+document.getElementById('pro_pic_upload').addEventListener('change', uploadProfilePic, false);
+document.getElementById('cover_pic_upload').addEventListener('change', uploadCoverPic, false);
