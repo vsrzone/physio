3db49002cfce6ad81e8e335cb02af84f1b44f21b
pulasthilakshan <pulasthilakshan@gmail.com>
2015-04-07 15:13:56 +0530

    resources added

---
+++ public/resources/pages/includes/header.php
@@ -0,0 +1,25 @@
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="index.html">Manaco Admin Panel</a>
+</div>
+<!-- /.navbar-header -->
+
+<ul class="nav navbar-top-links navbar-right">
+    <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
+        </a>
+        <ul class="dropdown-menu dropdown-user">
+            <li><div id="logout_button" onclick="requestLogout()" style="cursor:pointer;"><i class="fa fa-sign-out fa-fw"></i> Logout</div>
+            </li>
+        </ul>
+        <!-- /.dropdown-user -->
+    </li>
+    <!-- /.dropdown -->
+</ul>
+<!-- /.navbar-top-links -->
