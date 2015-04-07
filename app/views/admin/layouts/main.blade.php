0c61e99ce714801ba80618a72bb90d4e548ffc63
buddhi1 <buddhiashan1@gmail.com>
2015-04-07 15:56:38 +0530

    temp474

--- app/views/admin/layouts/main.blade.php
+++ app/views/admin/layouts/main.blade.php
@@ -2,23 +2,10 @@
 <html lang="en">
 
 	<head>
-<<<<<<< HEAD
+
 		<meta charset="utf-8">
 
-		<link href="{{URL::to('/')}}/resources/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-
-	    <!-- MetisMenu CSS -->
-	    <link href="{{URL::to('/')}}/resources/bower_components/metisMenu/dist/metisMenu.min.css" rel="stylesheet">
-
-	    <!-- Custom CSS -->
-	    <link href="{{URL::to('/')}}/resources/dist/css/sb-admin-2.css" rel="stylesheet">
-
-	    <!-- Custom Fonts -->
-	    <link href="{{URL::to('/')}}/resources/bower_components/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-		<title>Physio - Admin panel</title>  	
-=======
-		<title>Physio - Admin panel</title> 
+		<title>Physio - Admin panel</title>  			
 
 	<link href="{{URL::to('/')}}/resources/bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
