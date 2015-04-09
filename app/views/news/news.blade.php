799a7325ea92ed4f12cc7e1139eb6b1cddf04268
buddhi1 <buddhiashan1@gmail.com>
2015-04-09 14:26:30 +0530

    summary field added

--- app/views/news/news.blade.php
+++ app/views/news/news.blade.php
@@ -17,7 +17,7 @@
 		<div class="col-xs-12 col-md-3">
 			<h3>Recent News</h3>
 			@foreach($all_news as $news_item)
-			<a href="{{ url() }}/news/{{$news_item->id}}"><p>{{$news_item->title}}</p></a>
+			<a href="{{ url() }}/news/{{$news_item->id}}"><p>{{$news_item->title}} ...</p></a>
 			@endforeach
 		</div>
 		<div class="col-xs-12 col-md-9" id="news-wrapper">
