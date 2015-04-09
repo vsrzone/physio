260e0f0a804d076d39089575cd4ba9fdb6009974
pulasthilakshan <pulasthilakshan@gmail.com>
2015-04-09 12:59:20 +0530

    home link added

--- app/views/admin/layouts/main.blade.php
+++ app/views/admin/layouts/main.blade.php
@@ -41,7 +41,9 @@
 				</div>
 				<!-- /.navbar-header -->
 		 	
+
 				<ul class="nav navbar-top-links navbar-right">
+					<a href = "{{URL::to('/')}}">Home</a>
 				    <li class="dropdown">
 				        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
 				            <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
