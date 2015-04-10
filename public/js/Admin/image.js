28c06a8dec0fc5fa31b27b123d3ffec8a12fd41d
vsrzone <vikumsri@gmail.com>
2015-04-10 14:23:22 +0530

    admin name change

--- public/js/Admin/image.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('news_images').addEventListener('change', handleProjectFileSelect, false);
-document.getElementById('add_news').addEventListener('click', uploadAlbum, false);
--- public/js/Admin/member.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('pro_pic_upload').addEventListener('change', uploadProfilePic, false);
-document.getElementById('cover_pic_upload').addEventListener('change', uploadCoverPic, false);
