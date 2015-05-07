6d0c7d468d158a17b2cf9b7b54fb23e420cae2ac
buddhi1 <buddhiashan1@gmail.com>
2015-05-07 10:08:13 +0530

    file add remove for lesson

--- app/controllers/LessonController.php
+++ app/controllers/LessonController.php
@@ -12,26 +12,14 @@ class LessonController extends BaseController{
 
 	//add function
 	public function postCreate(){
-
-		$validator = Validator::make(Input::all(), Lesson::$rules);
-
-		if($validator->passes()){
-
 			$lesson = new Lesson;
 			$lesson->topic = Input::get('topic');
 			$lesson->content = Input::get('content');
 			$lesson->user_id = Auth::id();
 			$lesson->save();
 
-			return Redirect::to('admin/lesson/create')
-					->with('message', 'New lesson has been added successfully');
+			return $lesson->id;
 		}
-
-		return Redirect::to('admin/lesson/create')
-				->with('message', 'Following errors occurred')
-				->withErrors($validator)
-				->withInput();
-	}
 
 	//view availabel lessons
 	public function getIndex(){
@@ -43,6 +31,13 @@ class LessonController extends BaseController{
 	public function postDestroy(){
 		$lesson = Lesson::find(Input::get('id'));
 		if($lesson){
+			while($attach = DB::table('attachments')->where('lesson_id', '=', $lesson->id)->first()){
+				$path = 'uploads/files/'.$attach->file;
+				if(file_exists($path)){
+					unlink($path);
+					DB::table('attachments')->where('id', '=', $attach->id)->delete();
+				}
+			}
 			$lesson->delete();
 
 			return Redirect::to('admin/lesson/index')
@@ -90,5 +85,84 @@ class LessonController extends BaseController{
 
 		return Redirect::to('admin/lesson/index')
 					->with('message', 'Something went worng. Please try again');
+	}
+
+	//uploads file to server and saves details to database
+	public function postFileupload(){
+		$target_dir = "uploads/files/";
+		$title = basename($_FILES["files"]["name"]);
+		$file_name = time().(Input::get('id')+1).'.'.pathinfo($title, PATHINFO_EXTENSION);
+		$target_file = $target_dir . $file_name;
+		$uploadOk = 1;
+		 // Check file size
+		if ($_FILES["files"]["size"] > 2000000) {
+		    echo "Sorry, your file is too large.";
+		    $uploadOk = 0;
+		}
+
+		// Check if $uploadOk is set to 0 by an error
+		if ($uploadOk == 0) {
+		    echo "Sorry, your file was not uploaded.";
+		// if everything is ok, try to upload file
+		} else {
+		    if (move_uploaded_file($_FILES["files"]["tmp_name"], $target_file)) {
+
+		        $attach = new Attachment;
+		        $attach->title = $title;
+		        $attach->file = $file_name;
+		        $attach->lesson_id = Input::get('lesson_id');
+		        $attach->save();
+
+		        return $attach->id;
+		    } else {
+		        echo "Sorry, there was an error uploading your file.";
+		    }
+		}
+	}
+
+	//to add more files to a lesson
+	public function postAddfiles(){
+		$lesson = Lesson::find(Input::get('id'));
+		if($lesson){
+			$attach = DB::table('attachments')->where('lesson_id', '=', $lesson->id)->get();
+			return View::make('admin.lesson.addfiles')
+				->with('lesson_id', $lesson->id)
+				->with('attaches', $attach);
+		}
+		
+	}
+
+	//removes uploaded files
+	public function postDeletefiles(){
+		$lesson = Lesson::find(Input::get('id'));
+		if($lesson){
+			$attach = DB::table('attachments')->where('lesson_id', '=', $lesson->id)->get();
+			return View::make('admin.lesson.removefiles')
+				->with('lesson_id', $lesson->id)
+				->with('attaches', $attach);
+		}
+	}
+
+	//remove existing files function
+	public function postDestroyfiles(){
+		$files = Input::get('files');
+		for ($i=0; $i < sizeof($files); $i++) { 
+			$attach = Attachment::find($files[$i]);
+			if($attach){
+				$attach->delete();
+				$path = 'uploads/files/'.$attach->file;
+				if(file_exists($path)){
+					unlink($path);
+				}
+				
+			}
+
+			return Redirect::to('admin/lesson/index')
+						->with('message', 'Files deleted successfully');			
+		
+		}
+		return Redirect::to('admin/lesson/index')
+						->with('message', 'No files selected');
+		
 	}
 }
--- app/views/admin/lesson/add.blade.php
+++ app/views/admin/lesson/add.blade.php
@@ -12,10 +12,13 @@
 	</ul>
 </div>
 @endif
-{{ Form::open(array('url'=>'admin/lesson/create')) }}
-<div> Lesson topic: {{ Form::text('topic', '', array('class'=>'form-control', 'required')) }} </div>
-<div>Content: {{ Form::textarea('content') }}</div>
-<div> {{ Form::submit('Add Lesson', array('class'=>'btn btn-default')) }} </div>
+{{ Form::open(array('url'=>'admin/lesson/create', 'files'=>true)) }}
+<div> Lesson topic: {{ Form::text('topic', '', array('class'=>'form-control', 'required', 'id'=>'topic')) }} </div>
+<div>Content: {{ Form::textarea('content', '', array('id'=>'content')) }}</div>
+<div> {{ Form::file('files[]', array('id'=>'file', 'multiple')) }} </div>
+<div id="file_upload"></div>
+<div id="uploadForm1"></div>
+<div> {{ Form::button('Add Lesson', array('class'=>'btn btn-default', 'id'=>'submit')) }} </div>
 {{ Form::close() }}
 
 <script src="//tinymce.cachefly.net/4.1/tinymce.min.js"></script>
@@ -29,6 +32,97 @@
         	"insertdatetime media table contextmenu paste"
 	    ],
 	 });
+
+</script>
+<script type="text/javascript">
+
+	var fileSelect = document.getElementById( "file" );
+	var uploadDiv = document.getElementById( "submit" );
+
+	document.getElementById('file').onchange = function(){
+		
+		uploadForm1.innerHTML = "";
+		for (var i = fileSelect.files.length - 1; i >= 0; i--) {
+			uploadForm1.innerHTML += fileSelect.files[i]["name"] +" ["+fileSelect.files[i]["size"]/1000000+ "Mb]" +"<br>";
+			if(fileSelect.files[i]["size"] >= 2000000){				
+				alert('Selected file is too large to upload');
+				document.getElementById('file').value = document.getElementById('file').defaultValue;
+				uploadForm1.innerHTML = "";				
+				break;
+			}
+		}		
+	}
+
+	document.getElementById('submit').onclick = function( event ) {
+	   
+	    uploadData();	    
+	};
+
+	var uploadData = function(){
+
+		var topic = document.getElementById('topic').value;
+		var content = tinyMCE.get('content').getContent();
+	
+		if(topic != "" && content != ""){
+			var formData = new FormData(  );
+        	var request = new XMLHttpRequest();
+
+			formData.append('topic', topic);
+			formData.append('content',content);
+			request.open( 'POST', '{{url()}}/admin/lesson/create', true );
+	        request.onload = function(  ) {
+	            if ( request.status === 200 ) {	
+	                if(request.responseText){
+	                	uploadFiles(request.responseText);
+	                }
+	            } else {
+	                console.log( "Error" );
+	            }
+	        };
+			request.send( formData );
+		}else{
+			alert('Topic and content fields are required.');
+			return false;
+		}
+		
+		
+	}
+
+	var uploadFiles = function(x) {
+	   	var lesson_id = x;
+	    var files = fileSelect.files;
+	    handleFile( files, 0 , lesson_id, 0);
+	};
+	function handleFile( files, index, x, y) {
+	  if(files.length <= 0){
+	  	return false;
+	  }
+	
+	  	var lesson_id = x;
+	    if( files.length > index ) {
+
+	        var formData = new FormData(  );
+	        var request = new XMLHttpRequest(  );
+
+	        formData.append( 'files', files[ index ] );
+	        formData.append( 'lesson_id', lesson_id );
+	        formData.append('id', y);
+	        formData.append( 'upload_submit', true );
+	        request.open( 'POST', '{{url()}}/admin/lesson/fileupload', true );
+	        request.onload = function(  ) {
+	            if ( request.status === 200 ) {
+	                console.log( "Uploaded" );
+	                
+	                handleFile( files, ++index, lesson_id, request.responseText);
+	            } else {
+	                console.log( "Error" );
+	            }
+	        };
+	        request.send( formData );
+	    }else{
+	    	window.location.assign('{{url()}}/admin/lesson/index');
+	   }    
+	}
 
 </script>
 @stop
