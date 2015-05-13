2a0fc91a427657d2273c4757ba07c5b74382e18d
buddhi1 <buddhiashan1@gmail.com>
2015-05-13 14:35:53 +0530

    add essay

--- app/controllers/EssayController.php
+++ app/controllers/EssayController.php
@@ -16,4 +16,28 @@ class EssayController extends BaseController{
 		return View::make('admin.paper.essay.add')
 				->with('examiners', $examiners);
 	}
+
+	public function getIndex() {
+		// This method will show all the questions in the database
+		// show 10 questions per page
+
+		$questions = DB::table('mcqs')
+					->where('type', '=', 2)
+					->paginate(10);
+		return View::make('admin.paper.essay.index')
+			->with('questions', $questions);
+	}
+
+	public function postEdit() {
+		// This method will show the question paper where user can edit anything he wants.
+
+		$id = Input::get('id');
+		$essay = Mcq::find($id);
+		if($essay){
+			return View::make('admin.paper.essay.edit')
+					->with('essay', $essay);
+		}
+		return Redirect::to('admin/paper/essay')
+					->with('message', 'Something went wrong. Please try again');
+	}
 }
