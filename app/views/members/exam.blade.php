9b10eed9dc150c8aad93787a73f6bae6733c7d92
pulasthilakshan <pulasthilakshan@gmail.com>
2015-05-15 13:09:45 +0530

    marks calculation done

--- app/views/members/exam.blade.php
+++ app/views/members/exam.blade.php
@@ -4,7 +4,7 @@
 <div id="timer"></div>
 <div id="clock"></div>
 
-{{ Form::open(array('url'=>'members/exam/markresults')) }}
+<!-- {{ Form::open(array('url'=>'members/exam/markresults')) }}
 {{ Form::hidden('paper_id', $exam->id) }}
 @foreach(json_decode($exam->paper) as $obj)
 	@foreach($obj as $cont)
@@ -18,19 +18,19 @@
 	@endforeach
 @endforeach
 
-{{ Form::submit('Submit Answers', array('id'=>'submit')) }}
-
-<form>
-
-	<p data-bind="text: question"></p>
-	<div data-bind="foreach: optionArray">
-		<input type="checkbox" data-bind="checked: state">
-		<p data-bind="text: text"></p>
+{{ Form::submit('Submit Answers', array('id'=>'submit')) }} -->
+	<input type = "hidden" id = "paper_id" value = "{{$exam->id}}">
+	<div data-bind="foreach: answerArray">
+		<h3 data-bind="text: question"></h3>
+		<div data-bind="foreach: optionArray">
+			<input type="checkbox" data-bind="checked: state">
+			<p style = "display: inline" data-bind="text: text"></p>
+			</br>
+		</div>
 	</div>
-	<div> <button type="submit">Submit Answers</button> </div>
-</form>
+	<div> <button onclick = "sendRequestToServerPost()">Submit Answers</button> </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
 <script type="text/javascript" src="{{url()}}/js/knockout-3.3.0.js"></script>
 <script type="text/javascript">
 
@@ -60,26 +60,30 @@
 	    }, 1000);
 	}
 
+
 	window.onload = function () {
 	    var minutes = {{$exam->duration}} * 60,
 	    display = document.querySelector('#timer');
 	    startTimer(minutes, display);
 	    loadPaper();
 	};
+	
+
 
 	//loading paper
 	var loadPaper = function(){
 		paper = {{$exam->paper}};
 		paper = paper.questions;
-		for (var j = paper.length - 1; j >= 0; j--) {
+		console.log(paper)
+		for (var j = 0; j <= paper.length - 1; j++) {
 		
 			var question = new Question();
 			question.question(paper[j].question);
 
-			for (var i = paper[j].options.length - 1; i >= 0; i--) {
+			for (var i = 0; i <= paper[j].options.length - 1; i++) {
 				
 				if(!(paper[j].options[i].text.trim() == '')){
-					option = new Option()
+					option = new Option();
 					option.text(paper[j].options[i].text);
 					option.state(false);
 					question.optionArray.push(option);
@@ -87,10 +91,6 @@
 			};	
 			questions.answerArray.push(question);
 		};
-	}
-
-	document.getElementById('submit').onclick = function(){
-		sendRequestToServerPost();
 	}
 
 	//option class
@@ -125,58 +125,27 @@
 
 		// send all the details to the server by an Ajax request
 
-		var title = document.getElementById('title').value;
-		var duration_hr = document.getElementById('duration_hr').value;
-		var duration_min = document.getElementById('duartion_min').value;
-		var rows = document.getElementsByName('examiners[]');
-		var description = document.getElementById('description').value;
-		var clean = cleanJson(savedQuestions);
-		var paper = ko.toJSON(clean);
-		var type = 2;
+		// var title = document.getElementById('title').value;
+		var answers = ko.toJSON(questions);
+		var paper_id = document.getElementById('paper_id').value;
 
-		var selectedRows = [];
-	    for (var i = 0, l = rows.length; i < l; i++) {
-	        if (rows[i].checked) {
-	            selectedRows.push(rows[i].value);
-	        }
-	    }
+		var headers = 'answers=' + answers + '&paper_id=' + paper_id;
 
-	    if(title !== '') {
+		var xmlhttp=new XMLHttpRequest();
+		
+		xmlhttp.onreadystatechange=function()
+		{
+			if (xmlhttp.readyState==4 && xmlhttp.status==200)
+			{
+	    		if(xmlhttp.responseText === 'success') {
+	    			window.location = "{{url()}}/members/exams";
+	    		}
+	    	}
+	  	}
 
-			if(duration_hr === '' && duration_min === '') {
-
-				alert('You Should Enter a Duration');
-			}
-			else {
-
-				if(!isNaN(duration_hr) && !isNaN(duration_min)) {
-
-					var headers = 'title=' + title + '&examiners=' + selectedRows + '&description=' + description + '&hours=' + duration_hr + '&mins=' + duration_min + '&paper=' + paper + '&type=' + type;
-
-					var xmlhttp=new XMLHttpRequest();
-					
-					xmlhttp.onreadystatechange=function()
-					{
-						if (xmlhttp.readyState==4 && xmlhttp.status==200)
-						{
-				    		if(xmlhttp.responseText === 'success') {
-				    			window.location = "{{url()}}/admin/paper/essay";
-				    		}
-				    	}
-				  	}
-
-					xmlhttp.open("POST","{{url()}}/members/exam/submit",true);
-					xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-					xmlhttp.send(headers);
-				} else {
-
-					alert('You Should Enter a Valid Duration');
-				}
-			}
-		} else {
-
-			alert('You Should Enter a Title');
-		}
+		xmlhttp.open("POST","{{url()}}/members/exam/markresults",true);
+		xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+		xmlhttp.send(headers);
 	}
 
 </script>
